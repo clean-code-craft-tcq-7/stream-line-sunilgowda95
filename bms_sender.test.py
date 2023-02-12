1f03a486e1097578f0af334560d5098936a2700f
hus6kor <sunilgowda95@gmail.com>
2023-02-12 12:28:18 +0530

    Adding KATA document consisting of requirement analysis, breakdown, addition of sender output JSON format document, and testing strategy

---
+++ bms_sender.test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class BmsSenderTest(object):
+    """ this class is to run test cases for bms sender """
+    def test_bms_sender(self):
+        return
+    
+if __name__ == "__main__":
+    unittest.main()
