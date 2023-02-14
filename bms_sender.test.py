eeeef1869d385d4322c788792026b049ee1e613a
hus6kor <sunilgowda95@gmail.com>
2023-02-14 12:39:30 +0530

    completed simulation module implementation

--- bms_sender.test.py
+++ bms_sender.test.py
@@ -1,8 +1,10 @@
+""" This file contains test methods """
 import unittest
 
 class BmsSenderTest(object):
     """ this class is to run test cases for bms sender """
     def test_bms_sender(self):
+        """ test bms_sender """
         return
     
 if __name__ == "__main__":
